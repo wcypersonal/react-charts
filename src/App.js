39c9d0f215097d9dabaa0cb2e2d3b61bbfa6efb9
wcypersonal <417177466@qq.com>
2019-03-20 20:19:10 +0800

    添加echarts pie

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import WaterPie from './water';
+import { Steps } from 'antd';
+// const SubMenu = Menu.SubMenu;
+const Step = Steps.Step;
 class App extends Component {
+  state = {
+    collapsed: false
+  }
+
+
+  toggleCollapsed = () => {
+    this.setState({
+        collapse: !this.state.collapsed
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+          <div style={{ width: 256 }}>
+              <Steps>
+                  <Step title="第一步" />
+                  <Step title="第二步" />
+                  <Step title="第三步" />
+              </Steps>
+              {/*<Button type="primary" onClick={this.toggleCollapsed} style={{ marginBottom: 16 }}>*/}
+                  {/*<Icon type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'} />*/}
+              {/*</Button>*/}
+              {/*<Menu*/}
+                  {/*defaultSelectedKeys={['1']}*/}
+                  {/*defaultOpenKeys={['sub1']}*/}
+                  {/*mode="inline"*/}
+                  {/*theme="dark"*/}
+                  {/*inlineCollapsed={this.state.collapsed}>*/}
+                  {/*<Menu.Item key="1">*/}
+                      {/*<Icon type="pie-chart" />*/}
+                      {/*<span>Option 1</span>*/}
+                  {/*</Menu.Item>*/}
+                  {/*<Menu.Item key="2">*/}
+                      {/*<Icon type="desktop" />*/}
+                      {/*<span>Option 2</span>*/}
+                  {/*</Menu.Item>*/}
+                  {/*<Menu.Item key="3">*/}
+                      {/*<Icon type="inbox" />*/}
+                      {/*<span>Option 3</span>*/}
+                  {/*</Menu.Item>*/}
+                  {/*<SubMenu key="sub1" title={<span><Icon type="mail" /><span>Navigation One</span></span>}>*/}
+                      {/*<Menu.Item key="5">Option 5</Menu.Item>*/}
+                      {/*<Menu.Item key="6">Option 6</Menu.Item>*/}
+                      {/*<Menu.Item key="7">Option 7</Menu.Item>*/}
+                      {/*<Menu.Item key="8">Option 8</Menu.Item>*/}
+                  {/*</SubMenu>*/}
+                  {/*<SubMenu key="sub2" title={<span><Icon type="appstore" /><span>Navigation Two</span></span>}>*/}
+                      {/*<Menu.Item key="9">Option 9</Menu.Item>*/}
+                      {/*<Menu.Item key="10">Option 10</Menu.Item>*/}
+                      {/*<SubMenu key="sub3" title="Submenu">*/}
+                          {/*<Menu.Item key="11">Option 11</Menu.Item>*/}
+                          {/*<Menu.Item key="12">Option 12</Menu.Item>*/}
+                      {/*</SubMenu>*/}
+                  {/*</SubMenu>*/}
+              {/*</Menu>*/}
+          </div>
+        {/*<WaterPie/>*/}
       </div>
     );
   }
